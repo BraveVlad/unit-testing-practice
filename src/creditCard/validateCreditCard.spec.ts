cbf536067d20d5c4aa12d8d374cc7a75a7965ed6
BraveVlad <giladvladpinker@gmail.com>
2024-06-05 02:56:58 +0300

    validateCreditCard spec: created failing unitest with mock valid and invalid numbers

---
+++ src/creditCard/validateCreditCard.spec.ts
@@ -0,0 +1,24 @@
+import { isValidCreditCardNumber } from "./validateCreditCard";
+
+fdescribe.each([
+	["3655952491", true],
+	["3655952492", false],
+
+	["378282246310005", true],
+	["378282246210005", false],
+
+	["6011111111111117", true],
+	["6011111111111107", false],
+
+	["4222222222222", true],
+	["42222222222222", false],
+
+	["0000000000000", true],
+	["1111111111111", false],
+])(`isValidCreditCardNumber`, (cardNumber, expected) => {
+	test(`Credit card: [${cardNumber}] should be ${
+		expected ? `valid` : `invalid`
+	} credit card`, () => {
+		expect(isValidCreditCardNumber(cardNumber)).toStrictEqual(expected);
+	});
+});
