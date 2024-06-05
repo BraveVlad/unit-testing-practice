8a3aa46704ee52180d139944086c33507278f3ea
BraveVlad <giladvladpinker@gmail.com>
2024-06-05 04:41:28 +0300

    isValidCreditCardNumber unitest : converted auto unittest to manually well described usecases

--- src/creditCard/validateCreditCard.spec.ts
+++ src/creditCard/validateCreditCard.spec.ts
@@ -1,35 +1,80 @@
 import { isValidCreditCardNumber } from "./validateCreditCard";
 
-fdescribe.each([
-	["12345674", true],
-	["00012345674", true],
-	["12435674", false],
-	["1234567", false],
+fdescribe("isValidCreditCardNumber", () => {
+	it("Valid card number: 4532015112830366", () =>
+		expect(isValidCreditCardNumber("4532015112830366")).toEqual(true));
 
-	["3655952491", true],
-	["3655952492", false],
+	it("Invalid card number: 4532015112830367", () =>
+		expect(isValidCreditCardNumber("4532015112830367")).toEqual(false));
 
-	["378282246310005", true],
-	["378282246210005", false],
+	it("Valid card number with spaces: 4532 0151 1283 0366", () =>
+		expect(isValidCreditCardNumber("4532 0151 1283 0366")).toEqual(true));
 
-	["6011111111111117", true],
-	["6011111111111107", false],
+	it("Valid card number with dashes: 4532-0151-1283-0366", () =>
+		expect(isValidCreditCardNumber("4532-0151-1283-0366")).toEqual(true));
 
-	["4222222222222", true],
-	["42222222222222", false],
+	it("Valid card number with mixed seperators: 4532-0151 1283 0366", () =>
+		expect(isValidCreditCardNumber("4532-0151 1283 0366")).toEqual(true));
 
-	["0000000000000", true],
-	["1111111111111", false],
+	it("Valid card number with newline and tab characters: 4532015\\t112830366\\n", () =>
+		expect(isValidCreditCardNumber("4532015\t112830366\n")).toEqual(true));
 
-	["1", false],
-	["0", false],
+	it("Invalid card number with incorrect length (15): 453201511283036", () =>
+		expect(isValidCreditCardNumber("453201511283036")).toEqual(false));
 
-	["42", true],
-	["43", false],
-])(`isValidCreditCardNumber`, (cardNumber, expected) => {
-	test(`Credit card: [${cardNumber}] should be ${
-		expected ? `valid` : `invalid`
-	} credit card`, () => {
-		expect(isValidCreditCardNumber(cardNumber)).toStrictEqual(expected);
-	});
+	it("Valid short card number: 79927398713", () =>
+		expect(isValidCreditCardNumber("79927398713")).toEqual(true));
+
+	it("Invalid short card number: 79927398710", () =>
+		expect(isValidCreditCardNumber("79927398710")).toEqual(false));
+
+	it("Valid card number with leading zeros: 0004532015112830366", () =>
+		expect(isValidCreditCardNumber("0004532015112830366")).toEqual(true));
+
+	it("Invalid card number with characters: 4532a0151b1283c0366", () =>
+		expect(isValidCreditCardNumber("4532a0151b1283c0366")).toEqual(false));
+
+	it("Only non-digit characters asdfgh", () =>
+		expect(isValidCreditCardNumber("asdfgh")).toEqual(false));
+
+	it("Only special characters !@#$%^%", () =>
+		expect(isValidCreditCardNumber(" !@#$%^%")).toEqual(false));
+
+	it("Empty string", () => expect(isValidCreditCardNumber("")).toEqual(false));
+
+	it("Invalid single zero digit", () =>
+		expect(isValidCreditCardNumber("0")).toEqual(false));
 });
+// fdescribe.each([
+// 	["12345674", true],
+// 	["00012345674", true],
+// 	["12435674", false],
+// 	["1234567", false],
+
+// 	["3655952491", true],
+// 	["3655952492", false],
+
+// 	["378282246310005", true],
+// 	["378282246210005", false],
+
+// 	["6011111111111117", true],
+// 	["6011111111111107", false],
+
+// 	["4222222222222", true],
+// 	["42222222222222", false],
+
+// 	["0000000000000", true],
+// 	["1111111111111", false],
+
+// 	["1", false],
+// 	["0", false],
+
+// 	["42", true],
+// 	["43", false],
+// ])(`isValidCreditCardNumber`, (cardNumber, expected) => {
+// 	test(`Credit card: [${cardNumber}] should be ${
+// 		expected ? `valid` : `invalid`
+// 	} credit card`, () => {
+// 		expect(isValidCreditCardNumber(cardNumber)).toStrictEqual(expected);
+// 	});
+// });
