de050327447fdb6fa2d3d1523529f4c9c0342426
BraveVlad <giladvladpinker@gmail.com>
2024-06-05 05:56:04 +0300

    removed comments

--- src/creditCard/validateCreditCard.ts
+++ src/creditCard/validateCreditCard.ts
@@ -1,9 +1,3 @@
-// TODO - FOCUS ON TDD .
-// [V] 0. Create a stub
-// [V] 1. Write a failing READABLE test
-// [ ] 2. Write code to make the test pass
-// [ ] 3. Repeat
-
 export function isValidCreditCardNumber(number: string) {
 	const cardNumber = number.replace(/[\s-]/g, "").split("").map(Number);
 	if (cardNumber.length < 2) return false;
