a54a14af0cbc0d141396c19c6f4ab01796c4b345
BraveVlad <giladvladpinker@gmail.com>
2024-06-05 04:42:08 +0300

    isValidCreditCardNumber unitest : added paypal's card numbers list as usecases. ! 4 usecases failed!

--- src/creditCard/validateCreditCard.spec.ts
+++ src/creditCard/validateCreditCard.spec.ts
@@ -45,36 +45,25 @@ fdescribe("isValidCreditCardNumber", () => {
 	it("Invalid single zero digit", () =>
 		expect(isValidCreditCardNumber("0")).toEqual(false));
 });
-// fdescribe.each([
-// 	["12345674", true],
-// 	["00012345674", true],
-// 	["12435674", false],
-// 	["1234567", false],
-
-// 	["3655952491", true],
-// 	["3655952492", false],
-
-// 	["378282246310005", true],
-// 	["378282246210005", false],
-
-// 	["6011111111111117", true],
-// 	["6011111111111107", false],
-
-// 	["4222222222222", true],
-// 	["42222222222222", false],
-
-// 	["0000000000000", true],
-// 	["1111111111111", false],
-
-// 	["1", false],
-// 	["0", false],
-
-// 	["42", true],
-// 	["43", false],
-// ])(`isValidCreditCardNumber`, (cardNumber, expected) => {
-// 	test(`Credit card: [${cardNumber}] should be ${
-// 		expected ? `valid` : `invalid`
-// 	} credit card`, () => {
-// 		expect(isValidCreditCardNumber(cardNumber)).toStrictEqual(expected);
-// 	});
-// });
+fdescribe.each([
+	["378282246310005", true],
+	["371449635398431", true],
+	["378734493671000", true],
+	["5610591081018250", true],
+	["30569309025904", true],
+	["38520000023237", true],
+	["6011111111111117", true],
+	["3530111333300000", true],
+	["3566002020360505", true],
+	["5555555555554444", true],
+	["5105105105105100", true],
+	["4111111111111111", true],
+	["4012888888881881", true],
+	["4222222222222", true],
+])(`isValidCreditCardNumber`, (cardNumber, expected) => {
+	test(`Credit card: [${cardNumber}] should be ${
+		expected ? `valid` : `invalid`
+	} credit card`, () => {
+		expect(isValidCreditCardNumber(cardNumber)).toStrictEqual(expected);
+	});
+});
